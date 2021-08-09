ad68d862efc621b6a1f5a3e26888c0cc14ca5d64
KOTIN\Administrator <1019268193@qq.com>
2021-08-09 20:53:05 +0800

    '首页顶部布局'

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import './assets/scss/base.scss'
+import './assets/css/normalize.css'
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/Main.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="topbar bg-black py-2 px-3 d-flex ai-center">
+      <img src="../assets/logo.png" height="35">
+      <div class="px-2 flex-1 left">
+        <div class="text-white">王者荣耀</div>
+        <div class="text-gray-1 fs-xxs">团队成就更多</div>
+      </div>
+      <button type="button" class="btn bg-primary">立即下载</button>
+    </div>
+    
+    <div class="bg-primary pt-3 pb-2">
+      <div class="nav d-flex text-white pb-1">
+        <div class="nav-item active">
+          <router-link class="nav-link" to="/" tag="div">首页</router-link>
+        </div>
+        <div class="nav-item">
+          <router-link class="nav-link" to="/" tag="div">攻略中心</router-link>
+        </div>
+        <div class="nav-item">
+          <router-link class="nav-link" to="/" tag="div">赛事中心</router-link>
+        </div>
+      </div>
+    </div>
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style type="scss">
+.topbar {
+  padding-bottom: 0.6154rem;
+}
+
+.topbar .left {
+    padding-top: 0.3846rem;
+}
+
+.nav {
+  justify-content: space-around;
+  padding-bottom: 0.3846rem;
+}
+
+</style>
